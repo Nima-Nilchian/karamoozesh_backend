a6d63fca1b0d64ca9eb8c344c5e1f08f114f25b9
amireza rezai <77775139+amirrezarezai@users.noreply.github.com>
2023-01-02 15:16:20 +0330

    update Resume models

--- Resume/models.py
+++ Resume/models.py
@@ -3,14 +3,27 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class Language(models.Model):
+    LEVEL_CHOICES = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    )
+    title = models.CharField(max_length=250)
+    level = models.CharField(max_length=1, choices=LEVEL_CHOICES)
+
+
 class Work(models.Model):
     title = models.CharField(max_length=250)
     company = models.CharField(max_length=250)
     industry = models.CharField(max_length=250)
-    start_date = models.DateField(blank=False)
-    end_date = models.DateField(blank=False)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True,null=True)
     until_now = models.BooleanField(default=False)
-    description = models.TextField(blank=False)
+    description = models.TextField(blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
 
@@ -20,7 +33,7 @@ class Education(models.Model):
     field_of_study = models.CharField(max_length=250)
     university = models.CharField(max_length=250)
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(blank=True,null=True)
     until_now = models.BooleanField(default=False)
     created_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
@@ -40,10 +53,10 @@ class Skill(models.Model):
 class Certificate(models.Model):
     title = models.CharField(max_length=250)
     institute = models.CharField(max_length=250)
-    file = models.FileField(blank=True,upload_to='/files/certificate/%Y/%d/%m/')
+    file = models.FileField(blank=True,upload_to='files/certificate/%Y/%d/%m/',null=True)
     issue_date = models.DateField()
-    expiration_date = models.DateField()
-    description = models.TextField()
+    expiration_date = models.DateField(blank=True)
+    description = models.TextField(blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
 
@@ -52,7 +65,7 @@ class Certificate(models.Model):
 class Project(models.Model):
     title = models.CharField(max_length=250)
     link = models.URLField()
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
 class Link(models.Model):
     title = models.CharField(max_length=250)
@@ -75,12 +88,19 @@ class Resume(models.Model):
         ('2', 'مجرد'),
         ('3', 'تمایل به اعلام ندارم'),
     )
+    CHARACTER_CHOICES = (
+        ('1','منزوی'),
+        ('2','دارای روحیه تیمی '),
+        ('3','متمایل به ارتباط'),
+        ('4','نداشتن تمایل به ارتباط')
+    )
     firstname = models.CharField(max_length=250)
     lastname = models.CharField(max_length=250)
     avatar = models.ImageField(upload_to='image/resume/avatar',blank=True)
     about_me = models.TextField()
     phone_number = models.BigIntegerField()
     gender = models.CharField(max_length=1,choices=GENDER_CHOICES)
+    character = models.CharField(max_length=100,choices=CHARACTER_CHOICES,blank=True)
     user_id = models.OneToOneField(User,on_delete=models.CASCADE,related_name='resume')
     duty_system = models.CharField(max_length=1,choices=DUTY_SYSTEM_CHOICES)
     martial_status = models.CharField(max_length=1,choices=MARTIAL_STATUS_CHOICES)
@@ -88,14 +108,15 @@ class Resume(models.Model):
     city = models.CharField(max_length=250)
     address = models.TextField(blank=True)
     suggested_salary = models.BigIntegerField(blank=True)
-    work_experience = models.ForeignKey(Work,related_name='resume',on_delete=models.CASCADE,blank=True)
-    education = models.ForeignKey(Education,related_name='resume',on_delete=models.CASCADE,blank=True)
-    skill = models.ForeignKey(Skill,related_name='resume',on_delete=models.CASCADE)
-    certificate = models.ForeignKey(Certificate,related_name='resume',on_delete=models.CASCADE)
-    link = models.ForeignKey(Link,related_name='resume',on_delete=models.CASCADE)
-    project = models.ForeignKey(Project,related_name='resume',on_delete=models.CASCADE)
-    # talent_survey = models.ForeignKey(Talent_survey,related_name='resume',on_delete=models.CASCADE)
-    file = models.FileField(upload_to='files/resume/%Y/%m/%d/',blank=True)
+    work_experience = models.ManyToManyField(Work,related_name='resume',blank=True,)
+    education = models.ManyToManyField(Education,related_name='resume',blank=True)
+    skill = models.ManyToManyField(Skill,related_name='resume')
+    certificate = models.ManyToManyField(Certificate,related_name='resume',blank=True)
+    link = models.ManyToManyField(Link,related_name='resume',blank=True)
+    project = models.ManyToManyField(Project,related_name='resume',blank=True)
+    language = models.ManyToManyField(Language,related_name='resume',blank=True)
+    # talent_survey = models.ManyToManyField(Talent_survey,related_name='resume',blank=True),
+    file = models.FileField(upload_to='files/resume/%Y/%m/%d/',blank=True,null=True)
     is_active = models.BooleanField(default=False)
     created_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
