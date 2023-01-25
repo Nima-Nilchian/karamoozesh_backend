43407ff51efd422a16e0cc9f8c3eadb6ccc616b7
arash <arashmoradi.programmer@gmail.com>
2023-01-25 22:21:06 +0330

    edit user directory

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'User',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'User.User'
+AUTH_USER_MODEL = 'user.user'
 
 
 # Internationalization
