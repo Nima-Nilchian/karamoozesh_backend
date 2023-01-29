7b6b68562bd77a3251d6776db25f6476e40fc702
sAm <m.a.mofidian@hotmail.co.uk>
2023-01-29 19:48:55 +0330

    register and customize user app admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from .models import User, Profile, FavoriteCVs, UserSurvey
 
-# Register your models here.
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+
+
+class FavoriteCVsInline(admin.TabularInline):
+    model = FavoriteCVs
+    extra = 1
+
+
+class SurveyInline(admin.TabularInline):
+    model = UserSurvey
+    extra = 1
+
+
+@register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'is_active')
+    list_display_links = ('id', 'email')
+    list_filter = ('is_active',)
+    list_editable = ('is_active',)
+    search_fields = ('email',)
+    inlines = (ProfileInline, SurveyInline, FavoriteCVsInline)
