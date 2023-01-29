19595bcc3445b22aa2071a811717c2f9c9fcf9f9
sAm <m.a.mofidian@hotmail.co.uk>
2023-01-29 11:15:26 +0330

    fix and debug models(user & talent_survey apps)

--- user/models.py
+++ user/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.hashers import make_password
 from django.db import models
+from custom_lib.models import BaseModel
+from talent_survey.models import TalentSurvey
 from cv.models import CV
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import RegexValidator, EmailValidator
@@ -46,13 +48,19 @@ class User(AbstractUser):
     objects = UserManager()
 
 
-class Profile(models.Model):
+class Profile(BaseModel):
     user_id = models.OneToOneField('User', on_delete=models.CASCADE, related_name='profile')
     image = models.ImageField(null=True, blank=True)  # should add upload to
     phone_number = models.CharField(max_length=20, null=True, blank=True, validators=[
         RegexValidator(regex="/^(09|\\+989)\\d{9}$/", message='the phone number is wrong!')])
 
 
-class FavoriteCVs(models.Model):
-    user_id = models.ManyToManyField('User', related_name='favorite_cv')
-    cv_id = models.ManyToManyField(CV, related_name='favorite_cv')  # import cv
+class FavoriteCVs(BaseModel):
+    user_id = models.ForeignKey('User', related_name='favorite_cv', on_delete=models.CASCADE)
+    cv_id = models.ForeignKey(CV, related_name='favorite_cv', on_delete=models.CASCADE)
+
+
+class UserSurvey(BaseModel):
+    user_id = models.ForeignKey('User', related_name='surveys', on_delete=models.CASCADE)
+    survey_id = models.ForeignKey(TalentSurvey, related_name='users', on_delete=models.CASCADE)
+    result = models.URLField()
