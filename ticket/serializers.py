cff7364ad071c474974f1e01aa50df6e179b1270
sAm <m.a.mofidian@hotmail.co.uk>
2023-07-08 11:10:10 +0330

    fix ticket app serializers (remove phone number)

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -12,16 +12,25 @@ class TicketCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ticket
-        fields = ['title', 'question', 'tags', 'skill_level', 'phone_number', 'meeting_date']
+        fields = ['title', 'question', 'tags', 'skill_level', 'meeting_date']
 
     def save(self, **kwargs):
-        ticket = Ticket.objects.create(title=self.validated_data['title'],
-                                       skill_level=self.validated_data.get('skill_level', None),
-                                       phone_number=self.validated_data.get('phone_number', None),
-                                       meeting_date=self.validated_data.get('meeting_date', None))
-        Question.objects.create(user_id=self.context['request'].user,
-                                question=self.validated_data['question'],
-                                ticket_id=ticket)
+        ticket = Ticket.objects.create(
+            title=self.validated_data['title'],
+            skill_level=self.validated_data.get('skill_level', None),
+            phone_number=self.validated_data.get('phone_number', None),
+            meeting_date=self.validated_data.get('meeting_date', None)
+        )
+        Question.objects.create(
+            user_id=self.context['request'].user,
+            question=self.validated_data['question'],
+            ticket_id=ticket
+        )
+
         for tag_name in self.validated_data.get('tags', None):
             Tag.objects.create(name=tag_name)
-        return Response({'message': 'ticket has successfully created!', 'ticket_id': ticket.id}, status.HTTP_201_CREATED)
+
+        return Response(
+            {'message': 'ticket has successfully created!', 'ticket_id': ticket.id},
+            status.HTTP_201_CREATED
+        )
