d18405364de5b4618b7d2d54abcc300c54dec4d3
nima nilchian <nima.nilchian@gmail.com>
2023-07-13 19:02:40 +0330

    create survey and updated permission class of cv

--- talent_survey/serializers.py
+++ talent_survey/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from user.models import UserSurvey
+
 from .models import TalentSurvey
 
 
@@ -6,3 +9,20 @@ class TalentSurveySerializer(serializers.ModelSerializer):
     class Meta:
         model = TalentSurvey
         fields = "__all__"
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        auth_header = request.headers.get('Authorization', None)
+        print(auth_header, 'Hiiii')
+        print('hiii')
+
+        if auth_header:
+            token = auth_header.split(' ')[1]
+            user = Token.objects.get(key=token).user
+            print(user.id)
+            survey = TalentSurvey.objects.create(**validated_data)
+            UserSurvey.objects.create(user_id=user, survey_id=survey, result=validated_data['link'])
+
+            return survey
+        else:
+            return serializers.ValidationError('Token not found')
--- talent_survey/views.py
+++ talent_survey/views.py
@@ -1,11 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
 from custom_lib.permissions import IsStaffOrReadOnly
 from .models import TalentSurvey
 from .serializers import TalentSurveySerializer
 
 
-class TalentSurveyList(generics.ListCreateAPIView):
-    queryset = TalentSurvey.objects.all()
+class TalentSurveyList(generics.CreateAPIView):
+    # queryset = TalentSurvey.objects.all()
     serializer_class = TalentSurveySerializer
-    permission_classes = (IsStaffOrReadOnly,)
-    search_fields = ('name',)
+    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsStaffOrReadOnly,)
+    # search_fields = ('name',)
+
+
