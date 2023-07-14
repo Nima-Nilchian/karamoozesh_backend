9edf5cbcc73791ac4e3dc236e85ccc4597b87d86
sAm <m.a.mofidian@hotmail.co.uk>
2023-07-14 13:07:31 +0330

    fix user app admin

--- user/admin.py
+++ user/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.admin import register
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Profile, FavoriteCVs, UserSurvey
+from django.contrib.auth.hashers import make_password
 
 
 class ProfileInline(admin.TabularInline):
@@ -19,10 +20,17 @@ class SurveyInline(admin.TabularInline):
 
 
 @register(User)
-class UserAdmin(UserAdmin):
+class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'is_active', 'is_consultant')
     list_display_links = ('id', 'email')
     list_filter = ('is_active',)
     list_editable = ('is_active',)
     search_fields = ('email',)
     inlines = (ProfileInline, SurveyInline, FavoriteCVsInline)
+
+    def save_model(self, request, obj, form, change):
+        """ Hash password manually (receive raw password in admin) """
+        raw_password = form.cleaned_data.get('password')
+        hashed_password = make_password(raw_password)
+        obj.password = hashed_password
+        super().save_model(request, obj, form, change)
