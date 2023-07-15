52ff30c96ab1ef51a54da10c3692a96031fb016b
nima nilchian <nima.nilchian2@gmail.com>
2023-07-15 18:23:32 +0330

    hotfix user/view

--- user/urls.py
+++ user/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
         views.UserTalentSurveysView.as_view(),
         name='user-talent-surveys'
     ),
-    path('upload-image/', views.ProfileImageView.as_view(), name='upload-user-image'),
+    path('upload_image/', views.ProfImageView.as_view(), name='upload-user-image'),
 
 ]
